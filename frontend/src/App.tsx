db35881feda1b5905b8e219f50d4972c0b74f6dc
Tehillahk <tehillahk@outlook.com>
2024-02-09 09:16:35 -0600

    added new text

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import Header from "./components/header"
+import Problems from "./components/problems"
 import Suggestions from "./components/suggestions"
 
 
 function App() {
   
   return (
-   <div className="container h-96 w-80  px-5 py-10">
+   <div className="container h-96 w-full  px-5 py-5">
     <Header />
     <div className="flex flex-col mt-5 ">
+      <Problems />
       <Suggestions />
     
 
--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,13 +1,22 @@
-const Header =()=>{
-    return(
+const Header = () => {
+    return (
         <div className="flex flex-col">
             <h1 className="mb-3 text-xl font-bold">Accessability Checker</h1>
             <div className="flex flex-row justify-between">
-                <div>
-                    score
+                <div className="">
+                    <div>Score</div>
+                    <div className="flex justify-center items-center glass rounded-full w-12 h-12  ">
+                        <p className="font-bold">8.5</p>
+                    </div>
                 </div>
                 <div>
-                    score list
+                    <div className="mb-1"> Score list</div>
+                
+                        <p className="font-bold">Readability - 8.5</p>
+                        <p className="font-bold">Alt Text - 8.5</p>
+
+                    
+
                 </div>
 
             </div>
