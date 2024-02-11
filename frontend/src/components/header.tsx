e2954c05462fc999d9851b595f43f44397583ac2
Tehillahk <tehillahk@outlook.com>
2024-02-11 15:07:34 -0600

    added functions

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,25 +1,49 @@
+import { useState } from "react";
+import { getUrl } from "../utils/chrome";
+
+function Stats() {
+    return (
+        <div className="flex flex-row justify-between">
+
+            <div className="">
+                <div>Score</div>
+                <div className="flex justify-center items-center bg-primary rounded-full w-12 h-12  ">
+                    <p className="font-bold">8.5</p>
+                </div>
+            </div>
+            <div>
+                <div className="mb-1"> Score list</div>
+
+                <p className="font-bold">Readability - 8.5</p>
+                <p className="font-bold">Alt Text - 8.5</p>
+
+
+
+            </div>
+
+        </div>
+
+    )
+}
+
 const Header = () => {
+    const [init, setInit] = useState<Boolean>(false)
     return (
         <div className="flex flex-col">
             <h1 className="mb-3 text-xl font-bold">Accessability Checker</h1>
-            <div className="flex flex-row justify-between">
-                <div className="">
-                    <div>Score</div>
-                    <div className="flex justify-center items-center bg-primary rounded-full w-12 h-12  ">
-                        <p className="font-bold">8.5</p>
-                    </div>
-                </div>
-                <div>
-                    <div className="mb-1"> Score list</div>
-                
-                        <p className="font-bold">Readability - 8.5</p>
-                        <p className="font-bold">Alt Text - 8.5</p>
+           
+            {!init ? ( <button
+                className="btn btn-secondary mt-5"
+                onClick={
+                    async ()=>{
+                        const url = await getUrl()
+                        setInit(true);
+                    }
+                }
+            >
+                Analyze website
+            </button>): (<Stats />)}
 
-                    
-
-                </div>
-
-            </div>
         </div>
     )
 }
---
+++ frontend/src/utils/checker.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+async function checker(url :string) {
+    try {
+        const response = await axios(``)  ///to backend server
+        const data = await response.data()
+        return data
+        
+    } catch (error) {
+        /* failed to get response from server for some reason */
+        return null
+    }
+    
+}
