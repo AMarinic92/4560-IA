44e63435a6d001a91e2b8da147850b707142a905
Tehillahk <tehillahk@outlook.com>
2024-02-12 10:31:01 -0600

    added website section

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -29,15 +29,22 @@ function Stats() {
 
 const Header = ({changeIsReady}:{changeIsReady:ChangeIsReady}) => {
     const [init, setInit] = useState<boolean>(false)
+    const [website,setWebsite] = useState<string|null>(null)
     return (
         <div className="flex flex-col">
             <h1 className="mb-3 text-xl font-bold">Accessability Checker</h1>
+            
+            <div className="flex flex-row">
+              <p>Checking</p>
+               <p className="font-bold">{website}</p>
+            </div>
            
             {!init ? ( <button
                 className="btn btn-secondary mt-5"
                 onClick={
                     async ()=>{
                         const url = await getUrl();
+                        setWebsite(url?url:null)
                         setInit(true);
                         changeIsReady(true);
                     }
