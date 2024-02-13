476632c9a30c3a076934cff841d7ed7ff808d5c4
Andrew Marinic <ummarin9@myumanitoba.ca>
2024-02-13 12:20:44 -0600

    parser a little more suffiticated. alt text check function now returns a list of errors

--- backend/htmlparser.py
+++ backend/htmlparser.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from accesError import accessError
 
 class HtmlParser:
     def __init__(self,html) :
@@ -15,13 +16,18 @@ class HtmlParser:
          return self.soup.find_all("img", attrs={"alt": ""})
     
     def check_alt_text(self):
+         out = []
          image_tags = self.get_image_tags()
          for image in image_tags:
               if(image['src'] == "" ):
-                   print("Image found with missing source, discarding")
+                    print("Image found with missing source, discarding")
+                    out.insert(accessError("Alt text with no source","Image or Alt Text",image))
               elif(image.has_attr('alt')):
-                   print("Alt text found on:",image['src']," with alt text:", image['alt'])
+                    print("Alt text found on:",image['src']," with alt text:", image['alt'])
               elif(image.has_attr('alt') and image['alt'] == ""):
                     print("alt attribute present but blank check for decorative")
-              else:
-                   print("MISSING ALT TEXT ATTRIBUTE",image)
+                    out.insert(accessError("Alt text blank, may be decroative","Image or Alt Text",image))
+              elif(not(image.has_attr('alt'))):
+                    print("MISSING ALT TEXT ATTRIBUTE",image)
+                    out.insert(accessError("Alt attribute missing, maybe decroative","Image or Alt Text",image))
+         return out
--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,9 @@
 from read import Read
 from htmlparser import HtmlParser
-from accesError import accessError
 
-html = Read.read_web_page("https://www.w3schools.com/python/python_comments.asp")
+html = Read.read_web_page("https://www.lingscars.com/")
 parse = HtmlParser(html)
 
-parse.check_alt_text()
-Dict = dict({'best': '1', 'boy': '2'})
-test = accessError("test","mc-test", Dict)
-print(test)
+errors = parse.check_alt_text()
+for error in errors:
+    print(error)
