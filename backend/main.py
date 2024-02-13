2044ebff982a222dc2f07ba7aec187108bcd328b
Andrew Marinic <ummarin9@myumanitoba.ca>
2024-02-12 22:23:04 -0600

    added a dso for errors

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,11 @@
 from read import Read
 from htmlparser import HtmlParser
-
+from accesError import accessError
 
 html = Read.read_web_page("https://www.w3schools.com/python/python_comments.asp")
 parse = HtmlParser(html)
 
 parse.check_alt_text()
-
-
+Dict = dict({'best': '1', 'boy': '2'})
+test = accessError("test","mc-test", Dict)
+print(test)
