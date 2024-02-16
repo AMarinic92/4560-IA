fa4982cf14128a7f658262391bd74ee3880af457
RozenNoureev <rozennoureev@gmail.com>
2024-02-16 13:22:12 -0600

    Nothing important

--- frontend/src/utils/checker.ts
+++ frontend/src/utils/checker.ts
@@ -1,14 +1,16 @@
-import axios from "axios";
+/* eslint-disable*/
 
-async function checker(url :string) {
-    try {
-        const response = await axios(``)  ///to backend server
-        const data = await response.data()
-        return data
+//import axios from "axios";
+
+// async function checker(url :string) {
+//     try {
+//         const response = await axios(``)  ///to backend server
+//         const data = await response.data()
+//         return data
         
-    } catch (error) {
-        /* failed to get response from server for some reason */
-        return null
-    }
+//     } catch (error) {
+//         /* failed to get response from server for some reason */
+//         return null
+//     }
     
-}
+// }
