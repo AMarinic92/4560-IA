aa6b068278c1beb67215fdf85c17883a51c6a547
Tehillahk <tehillahk@outlook.com>
2024-02-25 15:25:58 -0600

    added flask

---
+++ backend/server.py
@@ -0,0 +1,49 @@
+
+import json
+
+from flask_restful.utils import cors
+
+
+
+from flask import Flask, request, render_template, abort
+
+from flask_restful import Resource, Api, reqparse
+from flask_caching import Cache
+
+
+from flask_cors import CORS
+
+config = {
+    "DEBUG": True,  # some Flask specific configs
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
+app = Flask(__name__)
+CORS(app)
+# tell Flask to use the above defined config
+app.config.from_mapping(config)
+cache = Cache(app)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+
+
+class Parse(Resource):
+    @cors.crossdomain(origin='*')
+    def post(self):
+        data = request.get_json(force=True)
+        print(data)
+
+    def get(self):
+        return "hello world"
+     
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template("index.html")
+
+
+api.add_resource(Parse, "/api/parse")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, threaded=True)
