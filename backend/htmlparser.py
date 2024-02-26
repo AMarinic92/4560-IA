bf345d948ea455f5f448633047ca8a3f243d5e3c
Tehillahk <tehillahk@outlook.com>
2024-02-25 22:10:58 -0600

    changed list

--- backend/htmlparser.py
+++ backend/htmlparser.py
@@ -21,11 +21,11 @@ class HtmlParser:
          id_count = 0
          for image in image_tags:
               if(image['src'] == "" ):
-                    out.append({id_count:{"type":"Alt text with no source","message":image["src"]}})
+                    out.append({"id":id_count,"type":"Alt text with no source","message":image["src"]})
               elif(image.has_attr('alt') and (image['alt'] == "" or image['alt'] == " ")):
-                    out.append({id_count:{"type":"Alt text blank, may be decorative","message":image["src"]}})
+                    out.append({"id":id_count,"type":"Alt text blank, may be decorative","message":image["src"]})
               elif(not(image.has_attr('alt'))):
-                    out.append({id_count:{"type":"Alt attribute missing, maybe decorative","message":image["src"]}})
+                    out.append({"id":id_count,"type":"Alt attribute missing, maybe decorative","message":image["src"]})
               elif(image.has_attr('alt')):
                     print("Alt text found on:",image['src']," with alt text:", image['alt'])
               id_count = id_count + 1
