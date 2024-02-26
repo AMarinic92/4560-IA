b501e20e8f8b86e2ac90548dea50d29ba5f18f6b
Tehillahk <tehillahk@outlook.com>
2024-02-25 22:10:58 -0600

    changed list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,33 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Header from "./components/header"
 import Problems from "./components/problems"
 import Suggestions from "./components/suggestions"
+import { Problem } from "./interfaces/objects";
 
 
 function App() {
 
-  const [isReady, setIsReady] = useState<boolean>(false)
+  const [isReady, setIsReady] = useState<boolean>(false);
+  const [problems, setProblems] = useState<Problem[]>([]);
+
+  const getProblems = async () => {
+    const response = await fetch("http://localhost:8080/api/parse");
+
+    if (response.ok) {
+      const results = await response.json();
+      return results.problems;
+    }
+
+    return null;
+
+  }
+
+  useEffect(
+    () => {
+
+
+    }, [problems]
+  )
 
   return (
     <div className="container h-96 w-full  px-5 py-5">
@@ -21,7 +42,7 @@ function App() {
       {
         isReady ? (<div className="flex flex-col mt-5 ">
 
-          <Problems />
+          <Problems problems={problems} />
           <Suggestions />
 
 
