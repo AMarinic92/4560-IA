84b02dcfe201bc3540af95dfeb51c1b0cafcc986
Andrew Z. Marinic <ummarin9@gallium-02.cs.umanitoba.ca>
2024-03-10 23:22:52 -0500

    got transformer working for quick easy ml

--- MachineLearning/MLmain.py
+++ MachineLearning/MLmain.py
@@ -8,6 +8,6 @@ import os.path
 
 
 test = Job("MLslurm",12,"docker run --runtime=nvidia --gpus 1 tensor-image",dict(time="12:0:0",output="ML.json",mailtype="BEGIN,END,FAIL",
-mem="4G",mailuser="ummarin9@myumanitoba.ca",gpus="1",gpumem="46068"))
+mem="48G",mailuser="ummarin9@myumanitoba.ca",gpus="1",gpumem="46068"))
 print(test.sbatchString())
 scheduleJob(test)
