4a4d2faa102940d40bedc0272a714bfdae3847eb
RozenNoureev <rozennoureev@gmail.com>
2024-03-11 00:56:58 -0500

    pivot to hugging face models.

--- MachineLearning/MLmain.py
+++ MachineLearning/MLmain.py
@@ -5,9 +5,11 @@ from job import Job
 from jobScheduler import scheduleJob
 import os.path
 
+from imageCaptioning import imageCaption
 
+# test = Job("MLslurm",12,"docker run --runtime=nvidia --gpus 1 tensor-image",dict(time="12:0:0",output="ML.json",mailtype="BEGIN,END,FAIL",
+# mem="48G",mailuser="ummarin9@myumanitoba.ca",gpus="1",gpumem="46068"))
+# print(test.sbatchString())
+# scheduleJob(test)
 
-test = Job("MLslurm",12,"docker run --runtime=nvidia --gpus 1 tensor-image",dict(time="12:0:0",output="ML.json",mailtype="BEGIN,END,FAIL",
-mem="48G",mailuser="ummarin9@myumanitoba.ca",gpus="1",gpumem="46068"))
-print(test.sbatchString())
-scheduleJob(test)
+imageCaption('https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg')
---
+++ MachineLearning/imageCaptioning.py
@@ -0,0 +1,26 @@
+# Code from https://huggingface.co/Salesforce/blip-image-captioning-base
+
+import requests
+from PIL import Image
+from transformers import BlipProcessor, BlipForConditionalGeneration
+
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base").to("cuda")
+
+def imageCaption(img_url):
+
+    raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
+
+    # conditional image captioning
+    text = "a photography of"
+    inputs = processor(raw_image, text, return_tensors="pt").to("cuda")
+
+    out = model.generate(**inputs)
+    print(processor.decode(out[0], skip_special_tokens=True))
+    # >>> a photography of a woman and her dog
+
+    # unconditional image captioning
+    inputs = processor(raw_image, return_tensors="pt").to("cuda")
+
+    out = model.generate(**inputs)
+    print(processor.decode(out[0], skip_special_tokens=True))
