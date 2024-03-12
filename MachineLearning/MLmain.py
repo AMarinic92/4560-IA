d6fa8e45e3d8af057520bad6929f185d4f9678c4
Andrew Marinic <ummarin9@myumanitoba.ca>
2024-03-11 21:05:33 -0500

    I have made a ml function that summarizes webpages

--- MachineLearning/MLmain.py
+++ MachineLearning/MLmain.py
@@ -1,13 +1,13 @@
 import sys
 sys.path.insert(0, '../backend')
 
-from job import Job
-from jobScheduler import scheduleJob
-import os.path
+from keywordfinder import KeyWorderFinder
 
 
+finder = KeyWorderFinder("https://en.wikipedia.org/wiki/Warhammer_40,000")
+keywords = finder.get_summary(1,10)
+print(keywords)
 
-test = Job("MLslurm",12,"docker run --runtime=nvidia --gpus 1 tensor-image",dict(time="12:0:0",output="ML.json",mailtype="BEGIN,END,FAIL",
-mem="48G",mailuser="ummarin9@myumanitoba.ca",gpus="1",gpumem="46068"))
-print(test.sbatchString())
-scheduleJob(test)
+#Tokenizer does not work
+"""keywords = finder.get_token_class()
+print(keywords)"""
