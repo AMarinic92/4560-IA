afb8ee3d2a0e0c7fe125d2e3110a5d2c43b7b875
Andrew Marinic <ummarin9@myumanitoba.ca>
2024-03-14 13:55:43 -0500

    done for the day

--- backend/socket/server.py
+++ backend/socket/server.py
@@ -1,5 +1,6 @@
 from aiohttp import web
 import socketio
+import json as Json
 
 sio = socketio.AsyncServer(async_mode='aiohttp',cors_allowed_origins='*')
 app = web.Application()
@@ -12,6 +13,17 @@ def connect(sid, environ, auth):
 @sio.on('parse')
 def another_event(sid, json):
     print("obj:", json)
+    asDict = Json.loads(json)
+    url = asDict.get("url", -1)
+    cmd = asDict.get("cmd", -1)
+    if(url != -1 and cmd != -1):
+        pass
+        #do the ml command here perhaps loop while it is doing its thing with timeout
+    else:
+        pass
+        #oopsie poopsie we need error handling
+
+    
 
 @sio.event
 def disconnect(sid):
