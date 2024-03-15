69f2508e700db6df58d2ae8ab308ff1878ef3ab3
Andrew Marinic <ummarin9@myumanitoba.ca>
2024-03-15 14:47:40 -0500

    made imageCaptioning not spaghetti and actually useful. Moved them to a folder called cmd inside socket. I dont think this is the right call but we need to put this somewhere it can easily access it. Maybe tehillah can help figure out the best way to strucutre this

--- MachineLearning/imageCaptioning.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-from transformers import pipeline
-
-captioner = pipeline("image-to-text",model="Salesforce/blip-image-captioning-base")
-out = captioner("https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/5/5e/Necron.jpg/revision/latest?cb=20220825124248")
-print(out)
