57a29f8c7ff30708e230e5a700bc410932944e5a
Tehillah <tehillahk@outlook.com>
2024-03-17 12:38:12 -0500

    port forwarded

--- backend/socket/clientSocket.py
+++ backend/socket/clientSocket.py
@@ -5,7 +5,7 @@ sio = socketio.AsyncSimpleClient()
 
 
 async def main():
-    await sio.connect('http://localhost:5000')
+    await sio.connect('http://localhost:5001')
     await sio.emit('parse',{"website":"google.com"})
     event = await sio.receive()
     print(f'received event: "{event[0]}" with arguments {event[1:]}')
