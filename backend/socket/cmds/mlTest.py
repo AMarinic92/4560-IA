f8c9f4d09a8ae7ceecc2adbade9e3d62895b0d9d
Andrew Marinic <ummarin9@myumanitoba.ca>
2024-03-25 19:46:14 -0500

    added some testing stuff and a pytorch image because tensor-flow appears to be slowing us down

---
+++ backend/socket/cmds/mlTest.py
@@ -0,0 +1,17 @@
+import imageCaptioning
+import tensorflow as tf
+from transformers import AutoTokenizer, TFAutoModelForTokenClassification, pipeline
+
+
+pipe = pipeline("image-classification", model="cafeai/cafe_aesthetic")
+result = pipe("https://static.wikia.nocookie.net/warhammer40k/images/5/55/AnkhoftheTriarch9thEdition.jpg/revision/latest/scale-to-width-down/1000?cb=20200925173557")
+print(result)
+'''
+tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
+model = TFAutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
+
+nlp = pipeline("ner", model=model, tokenizer=tokenizer)
+testIn = "The first match between the association football teams of Scotland and Wales took place on 25 March 1876 at Hamilton Crescent, Partick, Scotland. The fixture was organised by Llewelyn Kenrick, who had founded the Football Association of Wales only a few weeks earlier. The Welsh team was selected after trial matches were held at the Racecourse Ground in Wrexham, Wales. Scotland, the more experienced team, dominated the match and had several chances to score in the first half. They had a goal disallowed after scoring directly from a corner kick, before taking the lead after 40 minutes through John Ferguson. In the early stages of the second half, Wales attempted to play more openly to find a goal, but the Scottish side took advantage of their opponent's inexperience and scored two further goals. Scotland added a fourth through Henry McNeil and claimed a victory in front of a crowd of around 17,000 people, a record for an international fixture at the time."
+result = nlp(testIn)
+print(result)
+'''
