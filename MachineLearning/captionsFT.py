20402c534364134c7e9615f44f0b872e3c18181f
RozenNoureev <rozennoureev@gmail.com>
2024-03-27 23:50:23 -0500

    FT ready to test

--- MachineLearning/captionsFT.py
+++ MachineLearning/captionsFT.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-from transformers import pipeline, AutoProcessor, TFAutoModel, TFBlipForConditionalGeneration, AutoTokenizer, DataCollatorWithPadding, TrainingArguments, Trainer
+from transformers import pipeline, AutoProcessor, TFAutoModel, TFBlipForConditionalGeneration, AutoTokenizer, TrainingArguments, Trainer
 from datasets import load_dataset, load_metric
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
@@ -12,55 +12,63 @@ import PIL.Image
 from datasets.utils.file_utils import get_datasets_user_agent
 
 
-checkpoint = 'Salesforce/blip-image-captioning-base'
-# model = TFAutoModelForSequenceClassification.from_pretrained(checkpoint)
-loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-# model.compile(optimizer='', loss=loss)
+checkpoint = 'Salesforce/blip-image-captioning-large'
+
 
 ## Load in the dataset to fine tune model, This case conceptual captions
 
-USER_AGENT = get_datasets_user_agent()
+# USER_AGENT = get_datasets_user_agent()
 
 
 
-def fetch_single_image(image_url, timeout=None, retries=0):
-    for _ in range(retries + 1):
-        try:
-            request = urllib.request.Request(
-                image_url,
-                data=None,
-                headers={"user-agent": USER_AGENT},
-            )
-            with urllib.request.urlopen(request, timeout=timeout) as req:
-                image = PIL.Image.open(io.BytesIO(req.read()))
-            break
-        except Exception:
-            image = None
-    return image
+# def fetch_single_image(image_url, timeout=None, retries=0):
+#     for _ in range(retries + 1):
+#         try:
+#             request = urllib.request.Request(
+#                 image_url,
+#                 data=None,
+#                 headers={"user-agent": USER_AGENT},
+#             )
+#             with urllib.request.urlopen(request, timeout=timeout) as req:
+#                 image = PIL.Image.open(io.BytesIO(req.read()))
+#             break
+#         except Exception:
+#             image = None
+#     return image
 
 
-def fetch_images(batch, num_threads, timeout=None, retries=0):
-    fetch_single_image_with_args = partial(fetch_single_image, timeout=timeout, retries=retries)
-    with ThreadPoolExecutor(max_workers=num_threads) as executor:
-        batch["image"] = list(executor.map(fetch_single_image_with_args, batch["image_url"]))
-    return batch
+# def fetch_images(batch, num_threads, timeout=None, retries=0):
+#     fetch_single_image_with_args = partial(fetch_single_image, timeout=timeout, retries=retries)
+#     with ThreadPoolExecutor(max_workers=num_threads) as executor:
+#         batch["image"] = list(executor.map(fetch_single_image_with_args, batch["image_url"]))
+#     return batch
 
 
-num_threads = 20
-dset = load_dataset("conceptual_captions", trust_remote_code=True)
-dset = dset.map(fetch_images, batched=True, batch_size=100, fn_kwargs={"num_threads": num_threads})
+# num_threads = 20
+# dset = load_dataset("conceptual_captions", trust_remote_code=True)
+# dset = dset.map(fetch_images, batched=True, batch_size=100, fn_kwargs={"num_threads": num_threads})
+dset = load_dataset("imagefolder", data_dir="./trainTest", split="train")
+
 
 # load model
 
-checkpoint = "Salesforce/blip-image-captioning-base"
 pipe = pipeline("image-to-text", model = checkpoint)
 # tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+processor = AutoProcessor.from_pretrained(checkpoint)
 
 # model = TFAutoModel.from_pretrained(checkpoint)
 model = TFBlipForConditionalGeneration.from_pretrained(checkpoint)
 
 ## 
+
+loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+model.compile(optimizer='adam', loss=loss)
+
+model.fit(
+    tf_train_dataset,
+    validation_data=tf_validation_dataset,
+    epochs=3
+    )
 
 training_args = TrainingArguments("test-trainer", evaluation_strategy="epoch")
 
@@ -81,8 +89,8 @@ predictions = trainer.predict()
 print(predictions.predictions.shape, predictions.label_ids.shape)
 
 metric = load_metric("conceptual_captions")
-# preds = np.argmax(predictions.predictions, axis=-1)
-# metric.compute(predictions=preds, references=predictions.label_ids)
+preds = np.argmax(predictions.predictions, axis=-1)
+metric.compute(predictions=preds, references=predictions.label_ids)
 
 trainer.train()
 
@@ -91,25 +99,18 @@ trainer.train()
 
 
 
+# dataset = 'conceptual_captions'
+# raw_datasets = load_dataset(dataset)
 
+# tokenizer = AutoTokenizer.from_pretrained("Salesforce/blip-image-captioning-base")
 
-dataset = 'conceptual_captions'
-raw_datasets = load_dataset(dataset)
+# def tokenize_function(example):
+#   return tokenizer(
+#       example["image_url"], example["caption"],truncation=True, max_length = 1024
+#   )
 
-tokenizer = AutoTokenizer.from_pretrained("Salesforce/blip-image-captioning-base")
-
-def tokenize_function(example):
-  return tokenizer(
-      example["image_url"], example["caption"],truncation=True, max_length = 1024
-  )
-
-tokenized_datasets = raw_datasets.map(tokenize_function)
+# tokenized_datasets = raw_datasets.map(tokenize_function)
 
 
 
 
-# model.fit(
-#     tf_train_dataset,
-#     validation_data=tf_validation_dataset,
-#     epochs=3
-#     )
