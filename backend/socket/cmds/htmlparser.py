c2d5c1d0d7c666b419649a99a37ec5a77d674884
Andrew Marinic <ummarin9@myumanitoba.ca>
2024-03-28 10:20:04 -0500

    added the ability to check for a list of words as links such as "click me"

--- backend/socket/cmds/htmlparser.py
+++ backend/socket/cmds/htmlparser.py
@@ -4,7 +4,20 @@ from accesError import accessError
 class HtmlParser:
     def __init__(self,html) :
         self.soup = BeautifulSoup(html, 'html.parser')
+        self.id_count = 0
 
+    def get_links(self):
+         return self.soup.find_all(["a"])
+    
+    def get_links_with_words(self, words):
+         out = []
+         links = self.get_links
+         for link in links:
+              link_word = link.get_text()
+              for word in words:
+                   if (word == link_word):
+                        out.append({"id":self.id_count,"text":link.get_text(),"type":"Link has non-descriptive text","href":link["href"]})
+                        self.id_count += 1
 
     def get_page_words(self):
           return self.soup.find_all(["p","h1","h2","h3"])
@@ -21,15 +34,14 @@ class HtmlParser:
     def check_alt_text(self):
          out = []
          image_tags = self.get_image_tags()
-         id_count = 0
          for image in image_tags:
               if(image['src'] == "" ):
-                    out.append({"id":id_count,"type":"Alt text with no source","img":image["src"]})
-                    id_count += 1
+                    out.append({"id":self.id_count,"type":"Alt text with no source","img":image["src"]})
+                    self.id_count += 1
               elif(image.has_attr('alt') and (image['alt'] == "" or image['alt'] == " ")):
-                    out.append({"id":id_count,"type":"Alt text blank, may be decorative","img":image["src"]})
-                    id_count += 1
+                    out.append({"id":self.id_count,"type":"Alt text blank, may be decorative","img":image["src"]})
+                    self.id_count += 1
               elif(not(image.has_attr('alt'))):
-                    out.append({"id":id_count,"type":"Alt attribute missing, maybe decorative","img":image["src"]})
-                    id_count += 1
+                    out.append({"id":self.id_count,"type":"Alt attribute missing, maybe decorative","img":image["src"]})
+                    self.id_count += 1
          return out
