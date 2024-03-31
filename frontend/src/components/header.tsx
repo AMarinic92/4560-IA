b2d24f9b9f037803b699632857610cae01fc6147
Tehilah <tehillahk@outlook.com>
2024-03-30 19:34:47 -0500

    fixed header things

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -10,16 +10,11 @@ const Header = ({ changeIsReady }: { changeIsReady: ChangeIsReady }) => {
 
 
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col justify-start ">
             <h1 className="mb-3 text-xl font-bold">Accessability Checker</h1>
-
-            <div className="flex flex-row mb-1 w-1/2 ">
-                <p>Checking</p>
-                <p className="font-bold text-ellipsis ">{website}</p>
-            </div>
-            <div className="flex flex-row flex-wrap justify-evenly">
-                <button
-                    className="btn btn-info mb-2"
+            <div className="mb-3">
+                <a
+                    className="link link-primary text-lg mb-3"
                     onClick={
                         () => {
                             const modal = document?.getElementById('info_modal') as HTMLDialogElement;
@@ -27,13 +22,18 @@ const Header = ({ changeIsReady }: { changeIsReady: ChangeIsReady }) => {
                         }
                     }
                 >
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                        <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clipRule="evenodd" />
-                    </svg>
 
 
-                    What is this?
-                </button>
+                    What is this extension?
+                </a>
+
+                <div className="flex flex-row   ">
+                    <p className="text-base">Checking:</p>
+                    <p className="font-bold text-ellipsis text-base">{website}</p>
+                </div>
+            </div>
+            <div className="flex flex-row flex-wrap justify-between">
+
 
                 <button
                     className="btn btn-secondary "
@@ -63,7 +63,7 @@ const Header = ({ changeIsReady }: { changeIsReady: ChangeIsReady }) => {
                 <button
                     className="btn btn-accent"
                     onClick={
-                        ()=>setInit(false)
+                        () => setInit(false)
                     }
                     disabled={!init}
                 >
